5007fb344d23506aae75c0bd561d331a5b953487
OctavioM3 <octaviom3@nycstudents.net>
2024-04-17 09:19:44 -0400

    guuiuiug

--- sql/src/components/HelloWorld.vue
+++ sql/src/components/HelloWorld.vue
@@ -9,8 +9,10 @@
 
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://shoxgvzvnshjjblidrda.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNob3hndnp2bnNoampibGlkcmRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIzMjMxNTgsImV4cCI6MjAyNzg5OTE1OH0.5Ixgcj16B0S8pklU2ZSUZRrVCjXvrY6EBbULeXOpr5Y"
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabasekey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNob3hndnp2bnNoampibGlkcmRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIzMjMxNTgsImV4cCI6MjAyNzg5OTE1OH0.5Ixgcj16B0S8pklU2ZSUZRrVCjXvrY6EBbULeXOpr5Y"
+
+
+const supabase = createClient(supabaseUrl, supabasekey);
 
 console.log(supabase)
 
@@ -20,11 +22,12 @@ const { insert } = await supabase
 
 console.log({insert})
 
-const { data } = await supabase.auth.signUp({
+let { data, error } = await supabase.auth.signUp({
   email: 'example@gmail.com',
+  password: 'ltcfvqaDZZmCAOUFPCUa'
 })
 
-console.log({data})
+console.log({data, error})
 </script>
 
 <style lang="scss" scoped>
