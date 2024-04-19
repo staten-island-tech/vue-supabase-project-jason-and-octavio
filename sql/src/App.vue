7eb70329eedc92c55d44224dcb101b050b05ae2f
OctavioM3 <octaviom3@nycstudents.net>
2024-04-19 09:22:07 -0400

    hkjkj

--- sql/src/App.vue
+++ sql/src/App.vue
@@ -1,13 +1,9 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
   <header>
-    <HomeView>
-      
-    </HomeView>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
