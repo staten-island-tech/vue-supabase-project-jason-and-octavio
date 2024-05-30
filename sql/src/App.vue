9b5f4beb89de6e83589ac3703617cce57a2e4a4c
js1247 <144455716+js1247@users.noreply.github.com>
2024-05-30 08:56:49 -0400

    856

--- sql/src/App.vue
+++ sql/src/App.vue
@@ -2,8 +2,8 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { onMounted, ref } from 'vue'
 import Account from './components/Account.vue'
-import Auth from './components/Auth.vue'
-import { supabase } from './supabase'
+import Auth from './components/SignIn.vue'
+// import { supabase } from './supabase'
 const session = ref()
 
 onMounted(() => {
